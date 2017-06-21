04eb6c0be3272a9bf6c4a198d72e8de2dbb0dfba
undecV <undecV@gmail.com>
2017-06-22 01:57:57 +0800

    Make header

--- HTTPServer.c
+++ HTTPServer.c
@@ -18,42 +18,6 @@
 #include "HTTPServer.h"
 #include "ContentType.h"
 
-void PANIC(char *msg);
-#define PANIC(msg){perror(msg);exit(-1);}
-
-#define DEFAULT_PORT 80
-#define RECEIVE_BUFFER_SIZE 1024
-#define SEND_BUFFER_SIZE 2048
-#define CONTENT_BUFFER_SIZE 1024
-#define PARSE_BUFFER_SIZE 1024
-
-#define GET_COMMAND 101
-#define BAD_REQUEST 901
-
-
-typedef struct HttpRequest{
-    char method[20];    /* The method of request */
-    char *path;         /* The file path */
-    char *content;      /* The file content */
-    int contentlength;  /* The length of content */
-    int contenttype;    /* The type of content */
-    int rangeflag;      /* Flag */
-    long rangestart;    /* The starting position of the data */
-    long rangeend;      /* The end of the data */
-    long rangetotal;    /* The total length of the data */
-    char *prefix;       /* The filename extension */
-    int responsecode;   /* State code */
-}HR;
-
-
-void* threadFunc(void *threadArgs);
-int getRequest(char *requestbuf, struct HttpRequest *httpRequest);
-void getCommand(int clientfd, struct HttpRequest *httpRequest);
-void responseCode(int clientfd, int code, struct HttpRequest *httpRequest);
-int transferfile(int clientfd, FILE *fp, int type, 
-                 int rangestart, int totallength);
-int sendData(int clientfd, char *buf, int length);
-
 
 int main(int argc, char *argv[]){
     int port = DEFAULT_PORT;    // Server port
